4d4811b17d90d05231c991df4ba48a94631d0768
Raj Karmakar <97rajmath@gmail.com>
2024-08-14 13:21:15 +0530

    Contract should be initialized with balance of 1 ether

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -4,8 +4,9 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Escrow {
   address public owner;
   uint public balance;
-  constructor() {
+
+  constructor() payable {
     owner = msg.sender;
-    balance = address(this).balance;
+    balance = msg.value;
   }
 }
--- test/escrow.js
+++ test/escrow.js
@@ -2,8 +2,10 @@
 const Escrow = artifacts.require("Escrow");
 contract("Escrow", accounts => {
 	let escrow;
+	const depositAmount = web3.utils.toWei("1", "ether"); // 1 Ether
+
 	beforeEach(async () => {
-		escrow = await Escrow.new({from: accounts[0]});
+		escrow = await Escrow.new({from: accounts[0], value: depositAmount});
 	});
 
 	it("Should initialize owner address", async () => {
@@ -11,8 +13,8 @@ contract("Escrow", accounts => {
 		assert.equal(owner.toString(), accounts[0]);
 	})
 
-	it("Should initialize with balance", async () => {
+	it("Should initialize with balance of 1 ether", async () => {
 		const balance = await escrow.balance();
-		assert.isAbove(parseInt(balance), 0, "Balance should be greater than 0");
+		assert.equal(balance, depositAmount, "Contract balance should be 1 Ether");
 	})
 })
